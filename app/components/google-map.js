3189c420f98d95b17db4d116c083fb1a8c6aec25
chalmie <blairchalmers@gmail.com>
2016-04-04 15:45:41 -0700

    messed around and failed with gmaps api

---
+++ app/components/google-map.js
@@ -0,0 +1,18 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  map: Ember.inject.service('google-map'),
+
+  actions: {
+    showMap(listing) {
+      console.log(listing);
+      debugger;
+      var container = this.$('.map-display')[0];
+      var options = {
+        center: this.get('map').center(listing.get('latitude'), listing.get('longitude')),
+        zoom: 15
+      };
+      this.get('map').findMap(container, options);
+    }
+  }
+});
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,10 +4,9 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
-    // Add options here
   });
 
-  app.import('bower_components/moment/moment.js')
+
   app.import('vendor/gmaps.js');
 
   return app.toTree();
